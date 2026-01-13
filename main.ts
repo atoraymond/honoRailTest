0ad179a9e5383ab99a632d3ef3ed6d20bb403609
atoraymond <kwamea236@protonmail.com>
2026-01-13 14:41:14 +0000

    new commit

--- main.ts
+++ main.ts
@@ -1,28 +1,16 @@
-import { Hono } from "@hono/hono";
-import type { Context } from "@hono/hono";
-
-const app = new Hono();
-
-app.get("/", function(ctx: Context){
-    console.log("Got / requested");
-    return ctx.text("Welcome to my text");
-});
-
-app.get("/hello", function(ctx: Context){
-    console.log("Got /hello requested");
-    return new Response("<h1>Hello world</h1>",{
+function App(req: Request){
+    return new Response("Welcome to my pages",{
         headers: {
             "content-type": "text/html"
-        },
+        }
     });
-});
+}
 
 const { serve } = Deno;
 
 const hostPort = {
-    hostname: Deno.networkInterfaces()[1].address,
+    hostname: "127.0.0.1",
     port: 7000
 }
 
-console.log(Deno.networkInterfaces())
-serve(hostPort, app.fetch);
+serve(hostPort, App);
