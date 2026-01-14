74cf1233c7d313fbebf76dfeb4fdb5acf20ef0ce
atoraymond <kwamea236@protonmail.com>
2026-01-14 17:10:30 +0000

    testing something

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
-function handler(req: Request): Response{
-    return new Response("Hello world");
-}
+import { Hono } from "@hono/hono";
+import { Context } from "@hono/hono";
 
-Deno.serve(handler);
+const app = new Hono();
+
+app.get("/", async function(ctx: Context){
+    const file = await Deno.readFile("./index.html");
+   return new Response(new TextDecoder().decode(file),{
+    headers:{
+        "content-type": "text/html"
+    }
+   }); 
+});
+
+Deno.serve(app.fetch);
